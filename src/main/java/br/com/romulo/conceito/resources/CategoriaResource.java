79576c98358153bf4426a165dcc258ba1ad274f2
romulosp <romulos.p@hotmail.com>
2019-04-14 23:51:25 -0300

    alteracao

---
+++ src/main/java/br/com/romulo/conceito/resources/CategoriaResource.java
@@ -0,0 +1,9 @@
+package br.com.romulo.conceito.resources;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController(value="/categoria")
+public class CategoriaResource {
+	
+	 
+}
