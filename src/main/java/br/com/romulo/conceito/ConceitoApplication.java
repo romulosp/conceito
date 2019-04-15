7346f0cb17b3a44ae75acadade12501a9e7e9c05
romulosp <romulos.p@hotmail.com>
2019-04-14 23:33:01 -0300

    as

---
+++ src/main/java/br/com/romulo/conceito/ConceitoApplication.java
@@ -0,0 +1,13 @@
+package br.com.romulo.conceito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConceitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConceitoApplication.class, args);
+	}
+
+}
